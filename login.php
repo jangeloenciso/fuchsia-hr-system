fe19a932a1ab690f8b03a30c294d0e4974465ef3
JMF <71302306+jfontz@users.noreply.github.com>
2022-10-26 15:50:44 +0800

    Added login.php and CSS login folder
    Currently, only the frontend is added Please confirm that the UI is similar to the intended login UI.

---
+++ login.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <title>Login Page</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link
+      rel="stylesheet"
+      type="text/css"
+      media="screen"
+      href="CSS/login.css"
+    />
+  </head>
+  <body>
+    <div class="main">
+      <div class="card-main">
+        <p style="font-size: 4vw; margin: 36px 0px -10px 30px">ANAK NG</p>
+        <div class="card-secondary">
+          <p style="font-size: 4vw; margin: 10px 0px 0px 30px;">FUCHSIA HR</p>
+          
+          <FORM METHOD="POST">
+
+            <!--username-->
+            <p style="font-family: 'Glacial Indifference'; color: aliceblue !important; margin: 0px 0px 0px 30px;">Username</p>
+            <center><input type="text" name="user" /></center>
+
+            <!--password-->
+            <p style="font-family: 'Glacial Indifference'; color: aliceblue !important; margin: 16px 0px 0px 30px;">Password</p>
+            <center><input type="password" name="pass" /></center>
+            
+            <!--submit button-->
+            <center><input type="SUBMIT" name="#" value="LOGIN" /><center>
+          </FORM>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
