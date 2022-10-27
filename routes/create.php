af6f1eeff059d565d3a5aec7a71be7292edb3dd9
JMF <71302306+jfontz@users.noreply.github.com>
2022-10-27 23:50:46 +0800

    create & update
    updated with frontend

--- routes/create.php
+++ routes/create.php
@@ -1,35 +1,38 @@
 <?php
-  $PageTitle="Add New Employee";
-  include_once('../templates/header.php'); 
-  require_once "../config.php";
+$PageTitle = "Add New Employee";
+$PageCss = '../CSS/create.css';
+include_once('../templates/header.php');
+require_once "../config.php";
 
-  if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  if (!get_magic_quotes_gpc()) {
+    $id = addslashes($_POST['id']);
+    $first_name = addslashes($_POST['first_name']);
+    $last_name = addslashes($_POST['last_name']);
+    $age = addslashes($_POST['age']);
+    $gender = addslashes($_POST['gender']);
+    $home_address = addslashes($_POST['home_address']);
+    $contact_number = addslashes($_POST['contact_number']);
+    $email_address = addslashes($_POST['email_address']);
+    $company_position = addslashes($_POST['company_position']);
+    $monthly_salary = addslashes($_POST['monthly_salary']);
+  } else {
+    $id = $_POST['id'];
+    $first_name = $_POST['first_name'];
+    $last_name = $_POST['last_name'];
+    $age = $_POST['age'];
+    $gender = $_POST['gender'];
+    $home_address = $_POST['home_address'];
+    $contact_number = $_POST['contact_number'];
+    $email_address = $_POST['email_address'];
+    $company_position = $_POST['company_position'];
+    $monthly_salary = $_POST['monthly_salary'];
   }
-  if($_SERVER["REQUEST_METHOD"] == "POST"){
-    if (! get_magic_quotes_gpc()) {
-      $first_name = addslashes($_POST['first_name']);
-      $last_name = addslashes($_POST['last_name']);
-      $age = addslashes($_POST['age']);
-      $gender = addslashes($_POST['gender']);
-      $home_address = addslashes($_POST['home_address']);
-      $contact_number = addslashes($_POST['contact_number']);
-      $email_address = addslashes($_POST['email_address']);
-      $company_position = addslashes($_POST['company_position']);
-      $monthly_salary = addslashes($_POST['monthly_salary']);
-    } else {
-      $first_name = $_POST['first_name'];
-      $last_name = $_POST['last_name'];
-      $age = $_POST['age'];
-      $gender = $_POST['gender'];
-      $home_address = $_POST['home_address'];
-      $contact_number = $_POST['contact_number'];
-      $email_address = $_POST['email_address'];
-      $company_position = $_POST['company_position'];
-      $monthly_salary = $_POST['monthly_salary'];
-    }
 
-    $sql = "INSERT INTO `employees` (
+  $sql = "INSERT INTO `employees` (
       `id` ,
       `first_name` ,
       `last_name` ,
@@ -42,7 +45,7 @@
       `monthly_salary`
       )
       VALUES (
-      NULL ,
+      '$id' ,
       '$first_name', 
       '$last_name', 
       '$age', 
@@ -55,40 +58,83 @@
       );";
 
 
-    if (mysqli_query($conn, $sql)) {
-      echo "New record created successfully";
-      exit();
-    } else {
-      echo "Error: " . $sql . "<br>" . $conn->error;
-    }
+  if (mysqli_query($conn, $sql)) {
+    echo "<p style='color: #205a68; font-family: 'League Spartan''>" . "New record created successfully" . "</p>";
+    exit();
+  } else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+  }
 
-    mysqli_close($conn);
-  } 
+  mysqli_close($conn);
+}
 ?>
 
-<h1>Add New Employee</h1>
-<form action="<?php $_SERVER["PHP_SELF"] ?>" method="post">
-  <input type="text" name="first_name" placeholder="First Name" required>
-  <br>
-  <input type="text" name="last_name" placeholder="Last Name" required>
-  <br>
-  <input type="text" name="age" placeholder="Age" required>
-  <br>
-  <input type="text" name="gender" placeholder="Gender" required>
-  <br>
-  <input type="text" name="home_address" placeholder="Address" required>
-  <br>
-  <input type="text" name="contact_number" placeholder="Contact Number" required>
-  <br>
-  <input type="text" name="email_address" placeholder="Email Address" required>
-  <br>
-  <input type="text" name="company_position" placeholder="Company Position" required>
-  <br>
-  <input type="text" name="monthly_salary" placeholder="Monthly Salary" required>
-  <br>
 
-  <input type="submit" name="submit" value="Submit">    
-</form>
+
+
+<div class="main">
+  <div class="main-top">
+    <p>ADD</p>
+  </div>
+  <form action="<?php $_SERVER["PHP_SELF"] ?>" method="post">
+    <center>
+      <p style="color: #fee9ee; font-family: 'League Spartan'!important;">EMPLOYEE DETAILS</p>
+    </center>
+    <div class="main-mid">
+
+      <div class="mid-left">
+        <p class="p-main-mid">First Name</p>
+        <input type="text" name="first_name" placeholder="First Name" required>
+
+        <p class="p-main-mid">Last Name</p>
+        <input type="text" name="last_name" placeholder="Last Name" required>
+
+        <p class="p-main-mid">Age</p>
+        <input type="text" name="age" placeholder="Age" required>
+      </div>
+
+      <div class="mid-right">
+        <p class="p-main-mid">Gender</p>
+        <input type="text" name="gender" placeholder="Gender" required>
+
+        <p class="p-main-mid">Address</p>
+        <input type="text" name="home_address" placeholder="Address" required>
+
+        <p class="p-main-mid">Phone</p>
+        <input type="text" name="contact_number" placeholder="Contact Number" required>
+      </div>
+    </div>
+
+    <center>
+      <p style="margin: 30px 0px 8px 0px; font-size: 15px;
+  font-family: 'Glacial Indifference';
+  color: aliceblue;">Employee ID</p>
+      <input type="text" name="id" placeholder="ID" required>
+    </center>
+<br /><br /><br /><br />
+    <center>
+      <p style="color: #fee9ee; font-family: 'League Spartan'!important;"> WORK INFORMATION </p>
+    </center>
+    <div class="main-bot">
+
+
+      <p class="p-main-bot">Email Address</p>
+      <input type="text" name="email_address" placeholder="Email Address" required>
+
+      <p class="p-main-bot">Company Position</p>
+      <input type="text" name="company_position" placeholder="Company Position" required>
+
+      <p class="p-main-bot">Monthly Salary</p>
+      <input type="text" name="monthly_salary" placeholder="Monthly Salary" required>
+
+
+      <input type="submit" name="submit" value="ADD">
+
+  </form>
+
+</div>
+<br /><br /><br />
+
 <?php
 include_once('../templates/footer.php');
 ?>
--- routes/update.php
+++ routes/update.php
@@ -1,5 +1,6 @@
 <?php
   $PageTitle="Update Record";
+  $PageCss = '../CSS/update.css';
   include_once('../templates/header.php'); 
   require_once "../config.php";
 
@@ -45,7 +46,7 @@ if (!$conn) {
 
     if (mysqli_query($conn, $sql)) {
       $home = 'http://localhost/fuchsia/index.php';
-      echo "Record updated successfully";
+      echo "<p style='color: #205a68; font-family: 'League Spartan''>" . "Record updated successfully" . "</p>";
       exit();
     } else {
       echo "Error: " . $sql . "<br>" . $conn->error;
@@ -55,32 +56,70 @@ if (!$conn) {
   } 
 ?>
 
-<h1>Update Record</h1>
-<form action="<?php $_SERVER["PHP_SELF"] ?>" method="post">
-  <input type="number" name="id" placeholder="Employee ID">
-  <br>
-  <input type="text" name="first_name" placeholder="First Name">
-  <br>
-  <input type="text" name="last_name" placeholder="Last Name">
-  <br>
-  <input type="text" name="age" placeholder="Age">
-  <br>
-  <input type="text" name="gender" placeholder="Gender">
-  <br>
-  <input type="text" name="home_address" placeholder="Address">
-  <br>
-  <input type="text" name="contact_number" placeholder="Contact Number">
-  <br>
-  <input type="text" name="email_address" placeholder="Email Address">
-  <br>
-  <input type="text" name="company_position" placeholder="Company Position">
-  <br>
-  <input type="decimal" name="monthly_salary" placeholder="Monthly Salary">
-  <br>
+<div class="main">
+  <div class="main-top">
+    <p>UPDATE</p>
+  </div>
+  <form action="<?php $_SERVER["PHP_SELF"] ?>" method="post">
+    <center>
+      <p style="color: #fee9ee; font-family: 'League Spartan'!important;">EMPLOYEE DETAILS</p>
+    </center>
+    <center>
+      <p style="margin: 30px 0px 8px 0px; font-size: 15px;
+  font-family: 'Glacial Indifference';
+  color: aliceblue;">Employee ID</p>
+      <input type="text" name="id" placeholder="ID" required>
+    </center>
+    <div class="main-mid">
+
+      <div class="mid-left">
+        <p class="p-main-mid">First Name</p>
+        <input type="text" name="first_name" placeholder="First Name" required>
+
+        <p class="p-main-mid">Last Name</p>
+        <input type="text" name="last_name" placeholder="Last Name" required>
+
+        <p class="p-main-mid">Age</p>
+        <input type="text" name="age" placeholder="Age" required>
+      </div>
+
+      <div class="mid-right">
+        <p class="p-main-mid">Gender</p>
+        <input type="text" name="gender" placeholder="Gender" required>
+
+        <p class="p-main-mid">Address</p>
+        <input type="text" name="home_address" placeholder="Address" required>
+
+        <p class="p-main-mid">Phone</p>
+        <input type="text" name="contact_number" placeholder="Contact Number" required>
+      </div>
+    </div>
+
+    
+<br /><br /><br /><br />
+    <center>
+      <p style="color: #fee9ee; font-family: 'League Spartan'!important;"> WORK INFORMATION </p>
+    </center>
+    <div class="main-bot">
 
 
-  <input type="submit" name="submit" value="Submit">    
-</form>
+      <p class="p-main-bot">Email Address</p>
+      <input type="text" name="email_address" placeholder="Email Address" required>
+
+      <p class="p-main-bot">Company Position</p>
+      <input type="text" name="company_position" placeholder="Company Position" required>
+
+      <p class="p-main-bot">Monthly Salary</p>
+      <input type="text" name="monthly_salary" placeholder="Monthly Salary" required>
+
+
+      <input type="submit" name="submit" value="COMMIT">
+
+  </form>
+
+</div>
+<br /><br /><br />
+
 <?php
 include_once('../templates/footer.php');
 ?>
