e7e9214c10da48f4bd6258df97885830f87fa609
Joseph Angelo Enciso <jangeloenciso@gmail.com>
2022-10-30 17:34:01 +0800

    Login functionality

--- login.php
+++ login.php
@@ -1,3 +1,46 @@
+<?php
+
+session_start();
+
+require_once "config.php";
+
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+ 
+    $username = $_POST["username"];
+
+    $password = $_POST["password"];
+
+    if (empty($username)) {
+      echo $errors . "Username is required";
+    }
+    if (empty($password)) {
+      echo $errors . "Password is required";
+    }
+  
+    $sql = "SELECT * FROM users WHERE username='$username' AND password='$password' LIMIT 1";
+    $results = mysqli_query($conn, $sql);
+    
+    if (mysqli_num_rows($results) == 1) {
+      $user = mysqli_fetch_assoc($results);
+      if ($user['user_type'] == 'admin'){
+        $_SESSION['user'] = $logged_user;
+        $_SESSION['success']  = "You are now logged in";
+        header('location: admin.php');	
+      } else {
+        $_SESSION['user'] = $logged_in_user;
+        $_SESSION['success']  = "You are now logged in";
+
+        header('location: employee.php');
+      }
+    } else {
+      echo "Invalid login credentials";
+    }
+
+    mysqli_close($conn);
+  }
+?>
+
+
 <!DOCTYPE html>
 <html>
   <head>
@@ -19,15 +62,15 @@
         <div class="card-secondary">
           <p style="font-size: 4vw; margin: 10px 0px 0px 30px;">FUCHSIA HR</p>
           
-          <FORM METHOD="POST">
+          <FORM action="<?php $_SERVER["PHP_SELF"] ?>" method="post">
 
             <!--username-->
             <p style="font-family: 'Glacial Indifference'; color: aliceblue !important; margin: 0px 0px 0px 30px;">Username</p>
-            <center><input type="text" name="user" /></center>
+            <center><input type="text" name="username" required/></center>
 
             <!--password-->
             <p style="font-family: 'Glacial Indifference'; color: aliceblue !important; margin: 16px 0px 0px 30px;">Password</p>
-            <center><input type="password" name="pass" /></center>
+            <center><input type="password" name="password" required/></center>
             
             <!--submit button-->
             <center><input type="SUBMIT" name="#" value="LOGIN" /><center>
