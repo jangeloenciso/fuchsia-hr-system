b41c78aae9f897ab1d61c46209e62ed61d17060b
JMF <71302306+jfontz@users.noreply.github.com>
2022-11-01 13:16:26 +0800

    revision

--- FirstRun.php
+++ FirstRun.php
@@ -3,4 +3,5 @@ include_once('misc/create_database.php');
 include_once('misc/create_table.php');
 include_once('misc/create_user_table.php');
 include_once('misc/insert_user_table.php');
+include_once('misc/delete_privilege_set.php');
 ?>
