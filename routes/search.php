642e53608ae6e8831cd706cf83e12c50d7114793
Joseph Angelo Enciso <jangeloenciso@gmail.com>
2022-12-13 01:07:31 +0800

    Search feature

--- routes/search.php
+++ routes/search.php
@@ -3,6 +3,11 @@ $PageTitle = "Search Employee";
 $PageCss = '../CSS/search.css';
 include_once('../templates/header.php');
 require_once "../config.php";
+
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+  $_SESSION['id'] = $_POST['id'];
+}
+
 ?>
 
 <div class="main">
@@ -10,18 +15,19 @@ require_once "../config.php";
     <p>SEARCH</p>
   </div>
   <div class="main-bot">
-
     <div class="card">
 
+    <form method="POST" action="searched_data.php">
       <center>
         <p style="margin: 30px 0px 8px 0px; font-size: 15px;
-  font-family: 'Glacial Indifference';
-  color: aliceblue;">Enter Employee ID</p>
+                  font-family: 'Glacial Indifference';
+                  color: aliceblue;"
+                  >Enter Employee ID</p>
         <input type="text" name="id" placeholder="ID" required>
 
         <input type="submit" name="submit" value="SEARCH">
       </center>
-
+    </form>
 
 
 
---
+++ routes/searched_data.php
@@ -0,0 +1,94 @@
+<?php
+$PageTitle = "Search Employee";
+$PageCss = '../CSS/view.css';
+include_once('../templates/header.php');
+require_once "../config.php";
+
+// Check connection
+$id = $_POST['id'];
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+  }
+  $sql = "SELECT id, first_name, last_name, company_position, monthly_salary, age, gender, contact_number, home_address, email_address  FROM employees WHERE id=$id";
+  $result = $conn->query($sql);
+
+
+  if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo '<center>';
+        echo '<div class="container">';
+
+        echo '<div class="id-top">';
+        echo 'ID: ' . $row['id'];
+        echo '</div>';
+
+        echo '<div class="container-mid">';
+        echo '<div class="mid-pic-name">';
+        echo '<img src="../img/profile.jpg">';
+        echo $row['first_name'] . ' ' . $row['last_name'];
+        echo '</div>';
+
+        echo '<div class="mid-mini1">';
+        echo '<div class="mini-1">';
+        echo 'Position: ';
+        echo '</div>';
+        echo '<div class="mini-2">';
+        echo $row['company_position'];
+        echo '</div>';
+        echo '</div>';
+
+        echo '<div class="mid-mini2">';
+        echo '<div class="mini-3">';
+        echo 'Monthly Salary: ';
+        echo '</div>';
+        echo '<div class="mini-4">';
+        echo $row['monthly_salary'];
+        echo '</div>';
+        echo '</div>';
+
+
+        echo '</div>';
+
+        echo '<div class="container-bot">';
+        echo '<div class="bot-1">';
+        echo 'Age: ' . '<mark class="mrk">'. $row['age'] . '</mark>';
+        echo '</div>';
+        echo '<div class="bot-2">';
+        echo 'Gender: ' . '<mark class="mrk">'. $row['gender'] . '</mark>';
+        echo '</div>';
+        echo '<div class="bot-3">';
+        echo 'Phone: ' . '<mark class="mrk">'. $row['contact_number'] . '</mark>';
+        echo '</div>';
+        echo '<div class="bot-4">';
+        echo 'Address: ' . '<mark class="mrk">'. $row['home_address'] . '</mark>';
+        echo '</div>';
+        echo '<div class="bot-5">';
+        echo 'Email: ' . '<mark class="mrk">'. $row['email_address'] . '</mark>';
+        echo '</div>';
+
+        echo "</div>";
+
+
+        echo "</div>";
+
+
+
+        echo '<a href="delete.php?id=' . $row['id'] . '"><button type="button" class="delete-btn">Delete</button></a>';
+        echo "<br />";
+        echo "<br />";
+        echo "<br />";
+        echo "<br />";
+        echo '</center>';
+    }
+  } else {
+    echo "0 resultaaas";
+    echo $id;
+  }
+  $conn->close();
+?>
+
+<?php
+include_once('../templates/footer.php');
+?>
